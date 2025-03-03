935606f3afc555f7259e9d40e3504f633f145735
an-k45 <an_k27@protonmail.com>
2025-03-03 12:34:35 -0500

    Template out page

--- src/routes/political-shifts/+page.svelte
+++ src/routes/political-shifts/+page.svelte
@@ -2,7 +2,26 @@
 </script>
 
 <main>
-    <h1>Political Shifts</h1>
+    <div class="title">
+        <h1>Immigrant Political Shifts</h1>
+        <h2>Changes in immigrant vote patterns in the GTA</h2>
+        <p>
+            Aniket Kali,
+            Serene Tan,
+            Jeff Allen
+            <br />
+            <br />
+            March 2025
+        </p>
+    </div>
+
+    <!-- Dual panel of constituency maps: vote share & immigrant percent -->
+
+    <!-- View correlation for any given election -->
+
+    <!-- View trend line of mean correlation (choose party) -->
+
+    <!-- View trend line mean vote share (choose party) for high immigrant ridings -->
 </main>
 
 <style>
