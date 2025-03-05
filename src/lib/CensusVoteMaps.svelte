9eeed5f5ae3b1dfd49fad76d3dfec8f82c1a7f95
an-k45 <an_k27@protonmail.com>
2025-03-05 13:17:53 -0500

    Template out map interactivity

--- src/lib/CensusVoteMaps.svelte
+++ src/lib/CensusVoteMaps.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import maplibregl from "maplibre-gl";
     import "maplibre-gl/dist/maplibre-gl.css";
+    import { FELXN_YEARS, ONTELXN_YEARS } from "../lib/constants.js";
 
     let map1, map2;
     const defaultCenter = [-79.3832, 43.6532];
@@ -16,6 +17,11 @@
     // Prevent infinite update loops
     let syncing = false;
 
+    let region = "fed";
+    let year = 2021;
+    let years = FELXN_YEARS;
+    let geoJsonData = null;
+
     function syncMaps(movingMap, targetMap) {
         movingMap.on("move", () => {
             if (!syncing) {
@@ -27,6 +33,28 @@
                 syncing = false;
             }
         });
+    }
+
+    function loadGeoJson() {
+        const filePath = `/data/elections/${region}_stats_${year}.geojson`;
+        fetch(filePath)
+            .then(response => response.json())
+            .then(data => {
+                geoJsonData = data;
+                console.log(geoJsonData);
+            });
+    }
+
+    function handleRegionChange(event) {
+        region = event.target.value;
+        years = region === "fed" ? FELXN_YEARS : ONTELXN_YEARS;
+        year = years[years.length - 1];
+        loadGeoJson();
+    }
+
+    function handleYearChange(event) {
+        year = event.target.value;
+        loadGeoJson();
     }
 
     onMount(() => {
@@ -53,8 +81,37 @@
         // Sync both maps
         syncMaps(map1, map2);
         syncMaps(map2, map1);
+
+        loadGeoJson();
     });
 </script>
+
+<div class="controls">
+    <select on:change={handleRegionChange}>
+        <option value="fed" selected>Federal</option>
+        <option value="ont-ed">Ontario</option>
+    </select>
+    <select on:change={handleYearChange}>
+        {#each years as y}
+            <option value={y} selected={y === year}>{y}</option>
+        {/each}
+    </select>
+</div>
+
+<div class="map-container">
+    <div class="map-section">
+        <div class="map-controls">
+            <select></select>
+        </div>
+        <div id="map1" class="map"></div>
+    </div>
+    <div class="map-section">
+        <div class="map-controls">
+            <select></select>
+        </div>
+        <div id="map2" class="map"></div>
+    </div>
+</div>
 
 <style>
     .map-container {
@@ -62,12 +119,23 @@
         gap: 10px;
     }
     .map {
-        width: 50%;
+        width: 100%;
         height: 500px;
     }
+    .controls {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        margin-bottom: 10px;
+    }
+    .map-controls {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 10px;
+    }
+    .map-section {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+    }
 </style>
-
-<div class="map-container">
-    <div id="map1" class="map"></div>
-    <div id="map2" class="map"></div>
-</div>
