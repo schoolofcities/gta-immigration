3f2f3bc9321351df024a00eb03b758d09d5acd96
an-k45 <an_k27@protonmail.com>
2025-04-14 16:03:51 -0400

    Revise map displays and add legend

--- src/lib/ImmigrationMap.svelte
+++ src/lib/ImmigrationMap.svelte
@@ -1,10 +1,6 @@
 <script>
-    // import ctData from "../assets/ctData.geo.json"; 
-    // import rinks from "../assets/toronto-rinks.geo.json";
     // import border from "../assets/toronto-former-municipal-boundaries.geo.json"
     // import municipalPoints from '../assets/toronto-former-municipal-points.geo.json';
-    // var ledColours = ["#ede9fe", "#dad9f9", "#aaacd4", "#6e6d9f", "#383669"] 
-    // export let demoGP;
     
     import { onMount } from "svelte";
     import { geoPath, geoMercator, scaleThreshold } from "d3"; 
@@ -19,33 +15,33 @@
             'breaks': [10, 20, 30, 40, 50, 60, 70],
             'breakSuffix': '%',
             'name': `Immigrant (% of population) in ${curYear}`,
-            'immDist': 0.0,
-            'notImmDist': 0.0,
-            'popDist': 0.0,
+            'immDist': 10.0,
+            'notImmDist': 10.0,
+            'popDist': 10.0,
         },
         1981: {
             'breaks': [10, 20, 30, 40, 50, 60, 70],
             'breakSuffix': '%',
             'name': `Immigrant (% of population) in ${curYear}`,
-            'immDist': 0.0,
-            'notImmDist': 0.0,
-            'popDist': 0.0,
+            'immDist': 10.0,
+            'notImmDist': 10.0,
+            'popDist': 10.0,
         },
         2001: {
             'breaks': [10, 20, 30, 40, 50, 60, 70],
             'breakSuffix': '%',
             'name': `Immigrant (% of population) in ${curYear}`,
-            'immDist': 0.0,
-            'notImmDist': 0.0,
-            'popDist': 0.0,
+            'immDist': 10.0,
+            'notImmDist': 10.0,
+            'popDist': 10.0,
         },
         2021: {
             'breaks': [10, 20, 30, 40, 50, 60, 70],
             'breakSuffix': '%',
             'name': `Immigrant (% of population) in ${curYear}`,
-            'immDist': 0.0,
-            'notImmDist': 0.0,
-            'popDist': 0.0,
+            'immDist': 10.0,
+            'notImmDist': 10.0,
+            'popDist': 10.0,
         },
     }
 
@@ -58,17 +54,11 @@
 
     let projection = $derived(
         geoMercator()
-            .center([-79.188 + 0.24 * ((600 - innerWidth) / 200), 43.727 - 0.02 * ((600 - innerWidth) / 200) ])
-            .scale([62000 * innerWidth / 600])
-            .angle([-17])
+            .center([-79.4, 43.7])
+            .scale([innerWidth * 30])
+            .translate([(innerWidth / 2) - 50, (height / 2) + 110])
+            .angle([-30])
     );
-
-    // let projection = $derived(
-    //     geoMercator()
-    //         .center([-79.4, 43.8]) // shifted west and slightly north
-    //         .scale([62000 * innerWidth / 3200]) // reduced from 62000 to 25000
-    //         .angle([-12]) // adjusted from -17 to -12 for GTA orientation
-    // );
     let path = $derived(geoPath(projection));
 
     // Other variables
@@ -78,8 +68,11 @@
 
     // Functions
     function getColour(percentage) {
-        // TODO: Add support for nan!!
-        if (percentage < 10) return colours[0];
+        if (percentage === null) {
+            return "url(#diagonalHatch)"; // For diagonal lines
+            // return "#cccccc"; // for simple light gray
+        }
+        else if (percentage < 10) return colours[0];
         else if (percentage < 20) return colours[1];
         else if (percentage < 30) return colours[2];
         else if (percentage < 40) return colours[3];
@@ -105,71 +98,35 @@
                 // Set the appropriate colour
                 data.features = data.features.map((feature) => {
                     feature.properties.colour = getColour(feature.properties.pct_imm);
-                    return feature
-                })
+                    return feature;
+                });
 
                 immData = data.features;
             });
     }
 
-    $effect(() => {
-        console.log(immData);
-    });
+    // $effect(() => {
+    //     console.log(immData);
+    // });
     
     onMount(() => {
         loadGeoJson();
     });
-
-    // let toRinks = rinks.features;
-    // let ct = ctData.features;
-
-    // ct.map((item =>{
-    //     item.properties[demoGP]
-    //     ?(item.properties["color_"+demoGP] = color(item.properties[demoGP]))
-    //     :(item.properties["color_"+demoGP] = "white");
-    // }))
-
-    // const demoGPs = {
-    //     "PopDen":{
-    //         "breaks":[2500,5000,7500],
-    //         "name":"Population Density (# of people per sq.km)",
-    //         "breakSuffix": "",
-    //         "barChartText": "overall population",
-    //         "walkTime": 34.3,
-    //         "transitTime": 23.4
-    //     },
-    //     "Immi%":{
-    //         "breaks":[30,45,60],
-    //         "name": "Immigrant (% of population)",
-    //         "breakSuffix": "%",
-    //         "barChartText": "immigrant population",
-    //         "walkTime": 37,
-    //         "transitTime": 24.5
-    //     },
-    //     "VM%":{
-    //         "breaks":[30,45,60],
-    //         "name": "Visible Minority (% of population)",
-    //         "breakSuffix": "%",
-    //         "barChartText": "visible minorities",
-    //         "walkTime": 39.7,
-    //         "transitTime": 25.4
-    //     },
-    //     "LIn%":{
-    //         "breaks":[5,10,15],
-    //         "name":"Low Income (% of population)",
-    //         "breakSuffix": "%",
-    //         "barChartText": "low-income residents",
-    //         "walkTime": 33.5,
-    //         "transitTime": 22.7
-    //     }
-    // }
 </script>
 
 
 
-<div id="container" bind:offsetWidth={divWidth}>
+<div class="map-inner-container" bind:offsetWidth={divWidth}>
 
 	<svg width={innerWidth} {height} id={curYear}>
+
+        <defs>
+            <pattern id="diagonalHatch" patternUnits="userSpaceOnUse" width="4" height="4">
+                <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" 
+                      stroke="#cccccc" 
+                      stroke-width="1" />
+            </pattern>
+        </defs>
 
         <text class="label" x="5" y="22">{mapConfig[curYear].name}</text>
         
@@ -183,17 +140,6 @@
 
         <!-- {#each border.features as data}
             <path d={path(data)} stroke="#1E3765" stroke-width=1 fill=none opacity=0.8/>
-        {/each} -->
-
-        <!-- {#each toRinks as data}
-            <circle
-            class="rink"
-            cx={projection(data.geometry.coordinates)[0]}
-            cy={projection(data.geometry.coordinates)[1]}
-            r="4"
-            stroke="white"
-            stroke-width="2"
-            fill="black"/>
         {/each} -->
 
         <!-- {#each municipalPoints.features as data}
@@ -218,49 +164,48 @@
 			</text>
 		{/each} -->
 
-        <!-- <rect class="box" width="40" height = "12" x="125" y="30" style="fill:{ledColours[3]}; stroke: white;"></rect>
-        <rect class="box" width="40" height = "12" x="85" y="30" style="fill:{ledColours[2]}; stroke: white;"></rect>
-		<rect class="box" width="40" height = "12" x="45" y="30" style="fill:{ledColours[1]}; stroke: white;"></rect>
-		<rect class="box" width="40" height = "12" x="5" y="30" style="fill:{ledColours[0]}; stroke: white;"></rect> -->
+        {#each colours as colour, i} 
+            <rect class="box" width="40" height="12" x={5 + (i * 40)} y="30" style="fill:{colours[i]}; stroke: white;"></rect>
+        {/each}
 
-        <!-- <text class="label legend" x="125" y="55" text-anchor="middle">{demoGPs[demoGP]["breaks"][2]+ demoGPs[demoGP].breakSuffix}</text>
-		<text class="label legend" x="85" y="55" text-anchor="middle">{demoGPs[demoGP]["breaks"][1]+ demoGPs[demoGP].breakSuffix}</text>
-		<text class="label legend" x="45" y="55" text-anchor="middle">{demoGPs[demoGP]["breaks"][0]+ demoGPs[demoGP].breakSuffix}</text> -->
+        {#each mapConfig[curYear].breaks as breakNum, i}
+            <text class="label legend" x={5 + ((i + 1) * 40)} y="55" text-anchor="middle">{mapConfig[curYear]["breaks"][i]+ mapConfig[curYear].breakSuffix}</text>
+        {/each}
         
     </svg>
 
-    <!-- <svg width={innerWidth} height="60">
+    <svg width={innerWidth} height="78">
 
-        <text class="label" x="5" y="{12}" text-anchor="start">Average travel time to nearest rink ({demoGPs[demoGP]["barChartText"]}):</text>
+        <text class="label" x="5" y="{12}" text-anchor="start">Mean distance to Toronto city center:</text>
 
-        <text class="label legend" x="46" y="{30}" text-anchor="end">Walk</text>
-        <rect class="bar" width="{250 * demoGPs[demoGP]["walkTime"] / 40}" height = "8" x="50" y="{22}" style="fill: #6D247A; stroke: white;"></rect>
-        <text class="label legend" x="{53 + 250 * demoGPs[demoGP]["walkTime"] / 40}" y="{30}" text-anchor="start">{demoGPs[demoGP]["walkTime"]} minutes</text>
-
-        <text class="label legend" x="46" y="{48}" text-anchor="end">Transit</text>
-        <rect class="bar" width="{250 * demoGPs[demoGP]["transitTime"] / 40}" height = "8" x="50" y="{40}" style="fill: #6D247A; stroke: white;"></rect>
-        <text class="label legend" x="{53 + 250 * demoGPs[demoGP]["transitTime"] / 40}" y="{48}" text-anchor="start">{demoGPs[demoGP]["transitTime"]} minutes</text>
-        
-    </svg> -->
+        {#each ['immDist', 'notImmDist', 'popDist'] as distKey, i}
+            <text class="label legend" x="100" y="{30 + (i * 18)}" text-anchor="end">
+                {#if distKey == 'immDist'}
+                    Immigrants
+                {:else if distKey == 'notImmDist'}
+                    Non-immigrants
+                {:else}
+                    Total pop.
+                {/if}
+            </text>
+            <rect class="bar" width="{250 * mapConfig[curYear][distKey] / 40}" height = "8" x="104" y="{22 + (i * 18)}" style="fill: #6D247A; stroke: white;"></rect>
+            <text class="label legend" x="{107 + 250 * mapConfig[curYear][distKey] / 40}" y="{30 + (i * 18)}" text-anchor="start">{mapConfig[curYear][distKey]} kilometers</text>
+        {/each}
+    </svg>
 
 </div>
 
 
 
 <style>
-    #container {
+    .map-inner-container {
         width: 100%;
         max-width: 600px;
         min-width: 400px;
     }
-    .rink{
-        stroke: var(--brandWhite);
-        stroke-width: 1px;
-        fill: var(--brandGray90)
-    }
     .ct {
         stroke: var(--brandWhite);
-        stroke-width: 0.1px;
+        stroke-width: 0.05px;
         opacity: 0.75;
     }
     .box {
--- src/routes/suburbanization/+page.svelte
+++ src/routes/suburbanization/+page.svelte
@@ -23,10 +23,20 @@
         </p>
     </div>
 
-    <!-- <ImmigrationMap curYear={1961} /> -->
-    <!-- <ImmigrationMap curYear={1981} /> -->
-    <!-- <ImmigrationMap curYear={2001} /> -->
-    <ImmigrationMap curYear={2021} />
+    <div class="map-grid">
+        <div class="map-outer-container">
+            <ImmigrationMap curYear={1961} />
+        </div>
+        <div class="map-outer-container">
+            <ImmigrationMap curYear={1981} />
+        </div>
+        <div class="map-outer-container">
+            <ImmigrationMap curYear={2001} />
+        </div>
+        <div class="map-outer-container">
+            <ImmigrationMap curYear={2021} />
+        </div>
+    </div>
 
     <div class="text">
         <h2>How do immigrants compare to the general population?</h2>
@@ -47,4 +57,27 @@
 </main>
 
 <style>
+    /* Default styles for the container and divs */
+    .map-grid {
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        max-width: 1200px;
+        gap: 20px;
+    }
+
+    .map-outer-container {
+        width: 100%; /* Default to 100% width on mobile */
+        box-sizing: border-box;
+        margin-top: 10px;
+        margin-bottom: 20px;
+    }
+
+    /* Media query for larger screens */
+    @media (min-width: 768px) {
+        .map-outer-container {
+            width: calc(50% - 10px); /* Two per row with gap */
+        }
+    }
 </style>
