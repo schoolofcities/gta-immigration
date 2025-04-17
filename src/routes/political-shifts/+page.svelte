59f99234dc5b87ae6a41a3761e32cf96fbbe3dd4
an-k45 <an_k27@protonmail.com>
2025-04-17 15:48:24 -0400

    Add metadata (no image and empty description)

--- src/routes/political-shifts/+page.svelte
+++ src/routes/political-shifts/+page.svelte
@@ -7,6 +7,32 @@
     import VoteShareCompare from '$lib/VoteShareCompare.svelte';
 </script>
 
+<svelte:head>
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, minimum-scale=1"
+	/>
+
+	<title>Changes in immigrant voting patterns in the Greater Toronto Area</title>
+
+	<meta name="description" content="Maps, charts, and rankings that compare 250 cities around the world">
+	<meta name="author" content="Aniket Kali, Serene Tan, & Jeff Allen">
+    
+	<meta property="og:title" content="Changes in immigrant voting patterns in the Greater Toronto Area" />
+	<meta property="og:description" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://schoolofcities.github.io/gta-immigration/political-shifts" />
+	<!-- <meta property="og:image" content="https://schoolofcities.github.io/essential-spaces/web-card.png" /> -->
+	<meta property="og:locale" content="en_CA">
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="https://schoolofcities.github.io/gta-immigration/political-shifts" />
+	<meta name="twitter:creator" content="@UofTCities" />
+	<meta name="twitter:title" content="Changes in immigrant voting patterns in the Greater Toronto Area" />
+	<meta name="twitter:description" content="" />
+	<!-- <meta name="twitter:image" content="https://schoolofcities.github.io/essential-spaces/web-card.png" />  -->
+</svelte:head>
+
 <main>
 
     <TopSofC/>
