0074f7d467f64b15f09d0f0f05da8102c4dfd907
an-k45 <an_k27@protonmail.com>
2025-07-03 17:06:26 -0400

    build

--- docs/_app/immutable/entry/start.DeCkOZ9U.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/CER5luQB.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,17 +16,17 @@
 	<!-- <meta name="author" content="Aniket Kali, Serene Tan, & Jeff Allen"> -->
     
 	<meta property="og:title" content="Immigrants in the Greater Toronto Area" />
-	<meta property="og:description" content="" />
+	<meta property="og:description" content="Politics, demography, and migration of the GTA's immigrants" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://schoolofcities.github.io/gta-immigration" />
 	<!-- <meta property="og:image" content="https://schoolofcities.github.io/essential-spaces/web-card.png" /> -->
 	<meta property="og:locale" content="en_CA">
 
-	<!-- <meta name="twitter:card" content="summary_large_image" /> -->
+	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="https://schoolofcities.github.io/gta-immigration" />
 	<meta name="twitter:creator" content="@UofTCities" />
 	<meta name="twitter:title" content="Immigrants in the Greater Toronto Area" />
-	<meta name="twitter:description" content="" />
+	<meta name="twitter:description" content="Politics, demography, and migration of the GTA's immigrants" />
 	<!-- <meta name="twitter:image" content="https://schoolofcities.github.io/essential-spaces/web-card.png" />  -->
 </svelte:head>
 
